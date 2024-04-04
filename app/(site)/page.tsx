d34ffcc9734550c9a894c1c97a16ecac3ae6b465
Suzy-Lee <>
2024-04-04 15:58:32 +0900

    feat: error page 추가

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+import { sleep } from '@/lib/utils';
+
+const page = async () => {
+  //await sleep(130000);
+
+  //throw new Error('Error from page.tsx');
+  return <div>HomePage</div>;
+};
+
+export default page;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+// Promise는 ms의 시간이 흐른 후에, setTimeout에 의해서 resolve된다.
