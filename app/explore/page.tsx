6a1b45aaae44d601e819b0d8b109e6ac4ed6660a
Suzy-Lee <>
2024-04-08 22:50:53 +0900

    feat: explore 경로의 카테고리 컴포넌트 추가

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import PagePadding from '@/components/PagePadding';
+import Category from './components/Category';
+import { getAllPlaylist, getSongListTop10 } from '@/lib/dummyData';
+import PlayListCarousel from '@/components/PlayListCarousel';
+import SongListCarousel from '@/components/SongListCarousel';
 
-const page = () => {
-  return <div>explore</div>;
+const page = async () => {
+  // const playlistArray = await getAllPlaylist();
+  // const songListTop10 = await getSongListTop10();
+
+  const [playlistArray, songListTop10] = await Promise.all([
+    getAllPlaylist(),
+    getSongListTop10(),
+  ]);
+
+  return (
+    <PagePadding>
+      <div className='mt-4'></div>
+      <Category />
+      <div className='mt-20'></div>
+      <PlayListCarousel playlistArray={playlistArray} title='새 앨범 및 싱글' />
+      <div className='mt-20'></div>
+      <SongListCarousel songListTop10={songListTop10} title='새 앨범 및 싱글 2' />
+    </PagePadding>
+  );
 };
 
 export default page;
