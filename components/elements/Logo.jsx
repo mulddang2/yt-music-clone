ba0a5bb195720336f8d18a1a98136f534385469a
Suzy-Lee <>
2024-04-08 22:43:23 +0900

    feat: drawer 컴포넌트 추가

--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -4,8 +4,9 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import IconButton from './IconButton';
+import { IoCloseOutline } from "react-icons/io5";
 
-const Logo = () => {
+const Logo = ({isInDrawer = false, onClickClose = () => {}}) => {
   const { push } = useRouter();
   const onClickLogo = () => {
     // home 이동 하는 로직
@@ -14,12 +15,16 @@ const Logo = () => {
 
   const onClickMenu = () => {};
 
+
   return (
     <section className='flex flex-row items-center gap-3'>
-      <IconButton
-        onClickIcon={onClickMenu}
-        icon={<RxHamburgerMenu size={24} />}
-      />
+      {isInDrawer ? <IconButton
+        onClickIcon={onClickClose}
+        icon={<IoCloseOutline size={30} />}
+      /> : <IconButton
+      onClickIcon={onClickMenu}
+      icon={<RxHamburgerMenu size={24} />}
+    />}
       <div className='cursor-pointer' onClick={onClickLogo}>
         <Image
           src={'/main-logo.svg'}
